14b94d0418f487c40aa18483ada840a702f576b2
NotMarra <70058346+NotMarra@users.noreply.github.com>
2025-12-14 01:37:46 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://wiki.notmarra.com",
   integrations: [
     starlight({
       title: "NotWiki",
